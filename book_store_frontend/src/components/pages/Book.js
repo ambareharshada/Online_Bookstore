8b874a859dc83de0e852f8cec9ddc0a92cd98c69
ambareharshada <ambareharshada88@gmail.com>
2024-07-25 15:38:58 +0530

    changes in book.js file added card style for add to cart functionality

--- book_store_frontend/src/components/pages/Book.js
+++ book_store_frontend/src/components/pages/Book.js
@@ -1,15 +1,14 @@
 import React from "react";
 import {
-  EditOutlined,
-  EllipsisOutlined,
-  SettingOutlined,
+  EyeOutlined,
+  HeartOutlined,
+  ShoppingCartOutlined,
 } from "@ant-design/icons";
-import { Avatar, Card } from "antd";
+import { Card } from "antd";
 import { Col, Row } from "antd";
 const style = {
-  background: "rgb(241, 142, 125)",
-  // padding: "8px 3px",
-  marginTop:"50px"
+  // background: "rgb(241, 142, 125)",
+  marginTop: "50px",
 };
 const { Meta } = Card;
 
@@ -25,13 +24,12 @@ function Book() {
         }}
       >
         <Col className="gutter-row" span={6}>
-          <div 
-          style={style}>
+          <div style={style}>
             {" "}
             <Card
               style={{
                 width: 250,
-                margin:"0px auto"
+                margin: "0px auto",
               }}
               cover={
                 <img
@@ -40,31 +38,326 @@ function Book() {
                 />
               }
               actions={[
-                <SettingOutlined key="setting" />,
-                <EditOutlined key="edit" />,
-                <EllipsisOutlined key="ellipsis" />,
+                <ShoppingCartOutlined key="Add To Cart" />,
+                <HeartOutlined key="Add To Wishlist" />,
+                <EyeOutlined />,
               ]}
             >
               <Meta
-                // avatar={
-                //   <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
-                // }
                 title="Exploring the Creative Mind: Thinking Outside the Box"
                 description="Business, Encyclopedias"
               />
-              <div class="authors-list">By <a href="https://hostacmee.space/demo/bookchoix/authors/ellie-thomson/" rel="tag">Ellie Thomson</a>, 
-              <a href="https://hostacmee.space/demo/bookchoix/authors/lucy/" rel="tag">Lucy</a></div>
+              <div class="authors-list">
+                By{" "}
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/ellie-thomson/"
+                  rel="tag"
+                >
+                  Ellie Thomson
+                </a>
+                ,
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/lucy/"
+                  rel="tag"
+                >
+                  Lucy
+                </a>
+              </div>
             </Card>
           </div>
         </Col>
         <Col className="gutter-row" span={6}>
-          <div style={style}>col-6</div>
+          <div style={style}>  <Card
+              style={{
+                width: 250,
+                margin: "0px auto",
+              }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://hostacmee.space/demo/bookchoix/wp-content/uploads/2021/03/creativity-572x764-1-550x680.jpg"
+                />
+              }
+              actions={[
+                <ShoppingCartOutlined key="Add To Cart" />,
+                <HeartOutlined key="Add To Wishlist" />,
+                <EyeOutlined />,
+              ]}
+            >
+              <Meta
+                title="Exploring the Creative Mind: Thinking Outside the Box"
+                description="Business, Encyclopedias"
+              />
+              <div class="authors-list">
+                By{" "}
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/ellie-thomson/"
+                  rel="tag"
+                >
+                  Ellie Thomson
+                </a>
+                ,
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/lucy/"
+                  rel="tag"
+                >
+                  Lucy
+                </a>
+              </div>
+            </Card></div>
         </Col>
         <Col className="gutter-row" span={6}>
-          <div style={style}>col-6</div>
+          <div style={style}>  <Card
+              style={{
+                width: 250,
+                margin: "0px auto",
+              }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://hostacmee.space/demo/bookchoix/wp-content/uploads/2021/03/creativity-572x764-1-550x680.jpg"
+                />
+              }
+              actions={[
+                <ShoppingCartOutlined key="Add To Cart" />,
+                <HeartOutlined key="Add To Wishlist" />,
+                <EyeOutlined />,
+              ]}
+            >
+              <Meta
+                title="Exploring the Creative Mind: Thinking Outside the Box"
+                description="Business, Encyclopedias"
+              />
+              <div class="authors-list">
+                By{" "}
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/ellie-thomson/"
+                  rel="tag"
+                >
+                  Ellie Thomson
+                </a>
+                ,
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/lucy/"
+                  rel="tag"
+                >
+                  Lucy
+                </a>
+              </div>
+            </Card></div>
         </Col>
         <Col className="gutter-row" span={6}>
-          <div style={style}>col-6</div>
+          <div style={style}>  <Card
+              style={{
+                width: 250,
+                margin: "0px auto",
+              }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://hostacmee.space/demo/bookchoix/wp-content/uploads/2021/03/creativity-572x764-1-550x680.jpg"
+                />
+              }
+              actions={[
+                <ShoppingCartOutlined key="Add To Cart" />,
+                <HeartOutlined key="Add To Wishlist" />,
+                <EyeOutlined />,
+              ]}
+            >
+              <Meta
+                title="Exploring the Creative Mind: Thinking Outside the Box"
+                description="Business, Encyclopedias"
+              />
+              <div class="authors-list">
+                By{" "}
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/ellie-thomson/"
+                  rel="tag"
+                >
+                  Ellie Thomson
+                </a>
+                ,
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/lucy/"
+                  rel="tag"
+                >
+                  Lucy
+                </a>
+              </div>
+            </Card></div>
+        </Col>
+      </Row>
+
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+      >
+        <Col className="gutter-row" span={6}>
+          <div style={style}>
+            {" "}
+            <Card
+              style={{
+                width: 250,
+                margin: "0px auto",
+              }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://hostacmee.space/demo/bookchoix/wp-content/uploads/2021/03/creativity-572x764-1-550x680.jpg"
+                />
+              }
+              actions={[
+                <ShoppingCartOutlined key="Add To Cart" />,
+                <HeartOutlined key="Add To Wishlist" />,
+                <EyeOutlined />,
+              ]}
+            >
+              <Meta
+                title="Exploring the Creative Mind: Thinking Outside the Box"
+                description="Business, Encyclopedias"
+              />
+              <div class="authors-list">
+                By{" "}
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/ellie-thomson/"
+                  rel="tag"
+                >
+                  Ellie Thomson
+                </a>
+                ,
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/lucy/"
+                  rel="tag"
+                >
+                  Lucy
+                </a>
+              </div>
+            </Card>
+          </div>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <div style={style}>  <Card
+              style={{
+                width: 250,
+                margin: "0px auto",
+              }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://hostacmee.space/demo/bookchoix/wp-content/uploads/2021/03/creativity-572x764-1-550x680.jpg"
+                />
+              }
+              actions={[
+                <ShoppingCartOutlined key="Add To Cart" />,
+                <HeartOutlined key="Add To Wishlist" />,
+                <EyeOutlined />,
+              ]}
+            >
+              <Meta
+                title="Exploring the Creative Mind: Thinking Outside the Box"
+                description="Business, Encyclopedias"
+              />
+              <div class="authors-list">
+                By{" "}
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/ellie-thomson/"
+                  rel="tag"
+                >
+                  Ellie Thomson
+                </a>
+                ,
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/lucy/"
+                  rel="tag"
+                >
+                  Lucy
+                </a>
+              </div>
+            </Card></div>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <div style={style}>  <Card
+              style={{
+                width: 250,
+                margin: "0px auto",
+              }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://hostacmee.space/demo/bookchoix/wp-content/uploads/2021/03/creativity-572x764-1-550x680.jpg"
+                />
+              }
+              actions={[
+                <ShoppingCartOutlined key="Add To Cart" />,
+                <HeartOutlined key="Add To Wishlist" />,
+                <EyeOutlined />,
+              ]}
+            >
+              <Meta
+                title="Exploring the Creative Mind: Thinking Outside the Box"
+                description="Business, Encyclopedias"
+              />
+              <div class="authors-list">
+                By{" "}
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/ellie-thomson/"
+                  rel="tag"
+                >
+                  Ellie Thomson
+                </a>
+                ,
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/lucy/"
+                  rel="tag"
+                >
+                  Lucy
+                </a>
+              </div>
+            </Card></div>
+        </Col>
+        <Col className="gutter-row" span={6}>
+          <div style={style}>  <Card
+              style={{
+                width: 250,
+                margin: "0px auto",
+              }}
+              cover={
+                <img
+                  alt="example"
+                  src="https://hostacmee.space/demo/bookchoix/wp-content/uploads/2021/03/creativity-572x764-1-550x680.jpg"
+                />
+              }
+              actions={[
+                <ShoppingCartOutlined key="Add To Cart" />,
+                <HeartOutlined key="Add To Wishlist" />,
+                <EyeOutlined />,
+              ]}
+            >
+              <Meta
+                title="Exploring the Creative Mind: Thinking Outside the Box"
+                description="Business, Encyclopedias"
+              />
+              <div class="authors-list">
+                By{" "}
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/ellie-thomson/"
+                  rel="tag"
+                >
+                  Ellie Thomson
+                </a>
+                ,
+                <a
+                  href="https://hostacmee.space/demo/bookchoix/authors/lucy/"
+                  rel="tag"
+                >
+                  Lucy
+                </a>
+              </div>
+            </Card></div>
         </Col>
       </Row>
     </div>
